f41737afb298df173effbd4db2ea337f682a147e
Dan Lindholm <main@xoudini.com>
2018-11-13 18:59:55 +0200

    Move environment config out of version control

--- src/liblr/settings.py
+++ src/liblr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import liblr.env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')cy7@+b=h+r1r5!)w5p-3+brppy6)q-62_m)m6aq5g_t_67m1='
+SECRET_KEY = liblr.env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,10 +76,7 @@ WSGI_APPLICATION = 'liblr.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': liblr.env.DATABASE
 }
 
 
