ea87cb0dabde32adf4ae69cc7d2bc4e2388ed6db
Dan Lindholm <main@xoudini.com>
2018-11-15 15:27:43 +0200

    Edit allowed hosts

--- src/liblr/settings.py
+++ src/liblr/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = liblr.env.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'schwetzen.com'
+]
 
 
 # Application definition
