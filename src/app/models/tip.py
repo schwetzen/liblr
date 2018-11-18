fc5333f24f4bb6e32bae7e346f1bda6c38ab1911
doc97 <daniel.riissanen@gmail.com>
2018-11-19 01:36:16 +0200

    Add verbose names to fields in ReadingTip model

--- src/app/models/tip.py
+++ src/app/models/tip.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class ReadingTip(models.Model):
-    title = models.CharField(max_length=30)
-    url = models.CharField(max_length=20)
-    description = models.CharField(max_length=50)
+    title = models.CharField(verbose_name="Title", max_length=30)
+    url = models.CharField(verbose_name="URL", max_length=20)
+    description = models.CharField(verbose_name="Description", max_length=50)
