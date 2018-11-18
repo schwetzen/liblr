06698b917dc55ed7ef122984bd52482e0c6280df
Dan Lindholm <main@xoudini.com>
2018-11-18 17:00:33 +0200

    Register url for registration and uncomment link in base

--- src/liblr/urls.py
+++ src/liblr/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     # Authentication
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
+    path('register', RegisterView.as_view(), name='register'),
 ]
