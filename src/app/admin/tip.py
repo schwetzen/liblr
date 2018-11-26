f171f9e76921c520c872d4ebfe907815fe71df7b
Dan Lindholm <main@xoudini.com>
2018-11-27 01:32:24 +0200

    Update admin declaration

--- src/app/admin/tip.py
+++ src/app/admin/tip.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 
 class ReadingTipAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'content_type', 'title', 'description',)
+    list_display = ('id', 'user', 'content_type', 'title', 'description', 'has_been_read', 'is_deleted',)
     ordering = ('-id',)
     search_fields = ('title', 'url', 'description',)
