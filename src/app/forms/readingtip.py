67985f7da695efa0ecbcccd5859b878a655fab68
Dan Lindholm <main@xoudini.com>
2018-11-27 01:33:25 +0200

    Add content fields to form

--- src/app/forms/readingtip.py
+++ src/app/forms/readingtip.py
@@ -6,3 +6,16 @@ class ReadingTipCreateForm(forms.ModelForm):
     class Meta:
         model = ReadingTip
         fields = ('title', 'content_type', 'description',)
+
+    isbn = forms.CharField(
+        label='ISBN',
+        max_length=20,
+        min_length=10,
+        required=False,
+    )
+
+    url = forms.CharField(
+        label='URL',
+        max_length=255,
+        required=False,
+    )
