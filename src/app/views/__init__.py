79e22c1275ad6cf5fd7a0c0137b1729a6a947582
sinyman <37442461+sinyman@users.noreply.github.com>
2018-12-02 20:53:35 +0200

    Added view and template for user settings

--- src/app/views/__init__.py
+++ src/app/views/__init__.py
@@ -1,5 +1,6 @@
 from app.views.auth import LoginView, LogoutView, RegisterView
 from app.views.index import IndexView
+from app.views.settings import AccountSettingsView
 from app.views.tips import (
     ReadingTipView, ReadingTipListView,
     ReadingTipCreateView, ReadingTipUpdateView
