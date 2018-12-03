1bc43983fb73b958122092cef0c56a059bbf5a21
sinyman <37442461+sinyman@users.noreply.github.com>
2018-12-03 21:23:16 +0200

    Updated placing of buttons on confirmation window and updated view

--- src/app/views/settings.py
+++ src/app/views/settings.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 from django.views.generic.edit import DeleteView
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 from app.models.user import User
 
@@ -8,15 +9,13 @@ from app.models.user import User
 class AccountSettingsView(TemplateView):
     template_name = "settings.html"
 
-class AccountDeleteView(DeleteView):
-    model = User
+class AccountDeleteView(TemplateView):
     template_name = "confirm_delete.html"
     
-    def delete(self, request):
-        user_to_delete = self.user
+    def post(self, *args, **kwargs):
+        user_to_delete = self.request.user
 
         user_to_delete.is_active = False
-        user_to_delete.is_deleted = True
         
         user_to_delete.save()
 
